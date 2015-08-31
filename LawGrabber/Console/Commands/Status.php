fc305134ba4d8d891c8b66213698ba7c14c0ec57
Alexander Shvets <neochief@shvetsgroup.com>
2015-08-31 21:19:53 +0300

    Fixed bug with the discovery.

--- LawGrabber/Console/Commands/Status.php
+++ LawGrabber/Console/Commands/Status.php
@@ -74,7 +74,7 @@ class Status extends Command
         }
 
         $output = [];
-        exec('pgrep -l -f "^php artisan start"', $output);
+        exec('pgrep -l -f "^php (.*?)artisan start"', $output);
         if (count($output)) {
             $currently_running = 'RUNNING';
         }
