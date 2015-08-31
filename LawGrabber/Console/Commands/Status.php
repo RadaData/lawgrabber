6a7c134a1fec9364625d4025508826bf72027d30
Alexander Shvets <neochief@shvetsgroup.com>
2015-09-01 00:42:52 +0300

    Updated status calculations.

--- LawGrabber/Console/Commands/Status.php
+++ LawGrabber/Console/Commands/Status.php
@@ -36,18 +36,23 @@ class Status extends Command
             $errors = substr_count(file_get_contents(log_path() . '/log.txt'), '|%|');
         }
 
-        $discovered_count = Law::count();
+        $cards_discovered = Law::count();
         $most_recent = Law::orderBy('date', 'desc')->take(1)->value('date');
         $most_recent_diff = floor((time() - (strtotime($most_recent)))/3600/24);
         $most_recent_age = $most_recent_diff ? $most_recent_diff . ' days ago' : 'up to date';
 
         $cards_downloaded = Law::where('status', Law::DOWNLOADED_CARD)->count();
-        $cards_downloaded_p = floor(($cards_downloaded / ($discovered_count ?: ($cards_downloaded ?: 1))) * 100);
-        $revisions_count = Revision::where('status', '<', Revision::NO_TEXT)->count();
+        $cards_downloaded_p = floor(($cards_downloaded / ($cards_discovered ?: ($cards_downloaded ?: 1))) * 100);
+        $cards_with_text = Law::where('has_text', Law::HAS_TEXT)->count();
+        $cards_without_text = Law::where('has_text', Law::NO_TEXT)->count();
+        $cards_errors = Law::where('status', Law::DOWNLOAD_ERROR)->count();
+
+        $revisions_count = Revision::count();
+        $revisions_with_text = Revision::where('status', '<', Revision::NO_TEXT)->count();
+        $revisions_without_text = Revision::where('status', Revision::NO_TEXT)->count();
+        $revisions_needs_update = Revision::where('status', Revision::NEEDS_UPDATE)->count();
         $revisions_downloaded = Revision::where('status', Revision::UP_TO_DATE)->count();
         $revisions_downloaded_p = floor(($revisions_downloaded / ($revisions_count ?: ($revisions_downloaded ?: 1))) * 100);
-
-        $cards_errors = Law::where('status', Law::DOWNLOAD_ERROR)->count();
         $revisions_errors = Revision::where('status', Revision::DOWNLOAD_ERROR)->count();
 
         $jobs_count = Job::where('finished', 0)->count();
@@ -89,11 +94,16 @@ class Status extends Command
         DB::commit();
 
         $errors = number_format($errors);
-        $discovered_count = number_format($discovered_count);
+        $cards_discovered = number_format($cards_discovered);
         $cards_downloaded = number_format($cards_downloaded);
-        $revisions_downloaded = number_format($revisions_downloaded);
-        $revisions_count = number_format($revisions_count);
+        $cards_with_text = number_format($cards_with_text);
+        $cards_without_text = number_format($cards_without_text);
         $cards_errors = number_format($cards_errors);
+        $revisions_count = number_format($revisions_count);
+        $revisions_with_text = number_format($revisions_with_text);
+        $revisions_without_text = number_format($revisions_without_text);
+        $revisions_downloaded = number_format($revisions_downloaded);
+        $revisions_needs_update = number_format($revisions_needs_update);
         $revisions_errors = number_format($revisions_errors);
         $jobs_count = number_format($jobs_count);
         $jobs_discovery = number_format($jobs_discovery);
@@ -106,16 +116,24 @@ class Status extends Command
         $status = <<<STATUS
 === Errors in log: {$errors}
 
-=== Discovered laws: {$discovered_count}
+=== Discovered laws: {$cards_discovered}
     Most recent law: {$most_recent} ({$most_recent_age})
 
 === Downloaded:
-         Cards: {$cards_downloaded} / {$discovered_count} ({$cards_downloaded_p}%)
-     Revisions: {$revisions_downloaded} / {$revisions_count} ({$revisions_downloaded_p}%)
+-------- Cards:
+         - all: {$cards_discovered}
+  - downloaded: {$cards_downloaded} ({$cards_downloaded_p}%)
+    - with-text {$cards_with_text}
+     - no-text: {$cards_without_text}
+      - errors: {$cards_errors}
 
-===     Errors:
-         Cards: {$cards_errors}
-     Revisions: {$revisions_errors}
+---- Revisions:
+         - all: {$revisions_count}
+   - with-text: {$revisions_with_text}
+     - no-text: {$revisions_without_text}
+  - up-to-date: {$revisions_downloaded} ({$revisions_downloaded_p}%)
+ - need-update: {$revisions_needs_update}
+      - errors: {$revisions_errors}
 
 === Jobs: {$currently_running}
     Todo: {$jobs_count} {$jobs_completion_time}
